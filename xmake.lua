a8c19fa7f6ca98e877119d2a5ddd1e8a628659a1
zeromake <a390720046@gmail.com>
2023-10-06 21:44:41 +0800

    fix: target

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,6 @@
 add_rules("mode.debug", "mode.release")
 
-target("gcvt")
+target("glib-gcvt")
     set_kind("$(kind)")
     add_files("src/gcvt.c")
     add_headerfiles("src/gcvt.h", {prefixdir = "glib"})
